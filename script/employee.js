c4fe1bc4ce2e007459aab7003e3c6a0cb99c0c82
Chelsey Morris <chelseymorris91@yahoo.com>
2021-03-11 20:29:34 -0800

    basic structure for script

---
+++ script/employee.js
@@ -0,0 +1,18 @@
+class Employee {
+    constructor(name, id, email,) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+
+    getName()
+
+    getID()
+
+    getEmail()
+
+    getRole()
+
+}
+
+module.exports = Employee;
